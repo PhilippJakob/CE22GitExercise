4a344be6b5fe6c7feb38774a925708a86d903207
pArbeit <pArbeit@dbserver.speedport.ip>
2021-06-01 22:46:29 +0200

    Startbutton added

--- Test2FX/src/fxPack/SampleController.java
+++ Test2FX/src/fxPack/SampleController.java
@@ -1,6 +1,16 @@
 package fxPack;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SampleController
 {
-   	
+   @FXML
+   private Button btStart;
+
+   @FXML
+   void start(ActionEvent event) {
+
+   }
 }
