8db454e2acb2b2fd595ba0440c621c2ec8b886c5
popov <popov@DESKTOP-4FHQ0EF.fritz.box>
2021-06-13 12:03:01 +0200

    Revert "DataBase: db_learningspaces_exercise"
    This reverts commit 66a021aef42a59811883d1594ed80243d297f6a1.

--- Test2FX/src/db_learningspaces_exercise.sql
+++ Test2FX/src/db_learningspaces_exercise.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `db_learningspaces_exercise` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `db_learningspaces_exercise`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
 -- Host: localhost    Database: db_learningspaces_exercise
@@ -98,4 +96,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-13 12:01:43
+-- Dump completed on 2021-06-13 11:49:20
